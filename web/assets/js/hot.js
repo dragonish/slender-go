a508a5cb91d4523dee15b9794fc7a50c2a07324b
dragonish <no.web.developer@outlook.com>
2024-06-17 19:27:17 +0800

    fix(hot): fix may not trigger access statistics
    this happens when an event is triggered by a child element under the `<a>` element

--- web/assets/js/hot.js
+++ web/assets/js/hot.js
@@ -5,14 +5,27 @@
   if (content) {
     content.addEventListener('click', e => {
       const target = e.target;
-      if (target && target.classList.contains('slender-bookmark-link')) {
-        const id = target.dataset.id;
-        if (id) {
-          fetch(`/api/v1/bookmarks/${id}/visits`, {
-            method: 'POST',
-          }).catch(err => {
-            console.error(err);
-          });
+      if (target) {
+        let linkEle = null;
+
+        if (target.classList.contains('slender-bookmark-link')) {
+          linkEle = target;
+        } else {
+          const parentEle = target.closest('.slender-bookmark-link');
+          if (parentEle) {
+            linkEle = parentEle;
+          }
+        }
+
+        if (linkEle) {
+          const id = linkEle.dataset.id;
+          if (id) {
+            fetch(`/api/v1/bookmarks/${id}/visits`, {
+              method: 'POST',
+            }).catch(err => {
+              console.error(err);
+            });
+          }
         }
       }
     });
