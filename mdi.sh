76a6cdc472aba6eb2d4d08221d9155e27dc4bac8
dragonish <no.web.developer@outlook.com>
2024-06-26 20:52:30 +0800

    chore(icons): print icon version after downloaded

--- mdi.sh
+++ mdi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+index_json="./assets/material-design-icons/index.json"
+index_js="./assets/material-design-icons/index.js"
+
 echo download material-design-icons...
-curl -o ./assets/material-design-icons/index.json https://registry.npmjs.org/@mdi/js/latest
-curl -o ./assets/material-design-icons/index.js https://gcore.jsdelivr.net/npm/@mdi/js@latest/mdi.js
+curl -o $index_json https://registry.npmjs.org/@mdi/js/latest
+curl -o $index_js https://gcore.jsdelivr.net/npm/@mdi/js@latest/mdi.js
 echo download done.
+
+if command -v jq &>/dev/null; then
+  echo "material-design-icons version: $(cat $index_json | jq '.version')"
+fi
--- si.sh
+++ si.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+index_json="./assets/simple-icons/index.json"
+index_js="./assets/simple-icons/index.js"
+
 echo download simple-icons file...
-curl -o ./assets/simple-icons/index.json https://registry.npmjs.org/simple-icons/latest
-curl -o ./assets/simple-icons/index.js https://gcore.jsdelivr.net/npm/simple-icons@latest/index.js
+curl -o $index_json https://registry.npmjs.org/simple-icons/latest
+curl -o $index_js https://gcore.jsdelivr.net/npm/simple-icons@latest/index.js
 echo download done.
+
+if command -v jq &>/dev/null; then
+  echo "simple-icons version: $(cat $index_json | jq '.version')"
+fi
