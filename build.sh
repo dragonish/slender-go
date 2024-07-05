04d1467af238cbde84e7b95a9d39b2630e8273bf
dragonish <no.web.developer@outlook.com>
2024-07-05 23:08:14 +0800

    chore(build): merge build script

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+read -n 1 -p "Would you want to export the image file? (Y/n) " ans;
+ans=${ans:-Y}
+
 VERSION=$(git describe --tags --abbrev=0)
 COMMIT=$(git rev-parse --short HEAD)
 
 DOCKERHUB_REPO="giterhub/slender"
 
+echo ""
 echo "Latest: $VERSION / $COMMIT"
 echo "Repo: $DOCKERHUB_REPO"
 
@@ -12,3 +16,11 @@ docker build -t "slender-base:$VERSION" -f docker/Dockerfile.base .
 docker build -t "$DOCKERHUB_REPO:latest" --build-arg BASE_IMAGE="slender-base:$VERSION" --build-arg VERSION="$VERSION" --build-arg COMMIT="$COMMIT" -f docker/Dockerfile.amd64 .
 
 docker images | grep "$DOCKERHUB_REPO"
+
+case $ans in
+  y|Y)
+    echo "export the image file..."
+    docker image save giterhub/slender:latest | gzip > slender.tar.gz
+    echo "export done."
+    ;;
+esac
--- save.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker image save giterhub/slender:latest | gzip > slender.tar.gz
